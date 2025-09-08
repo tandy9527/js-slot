4cd459b3cf81de9d719c06b176a0572f0a4375ae
tandy9527 <tengyi555888@gmail.com>
2025-09-08 20:39:28 +0800

    33333

--- core/data.go
+++ core/data.go
@@ -10,14 +10,14 @@ import (
 
 // 游戏数据结构--BalanceChangeData 余额变动
 type BalanceChangeData struct {
-	UID           int64  // 用户ID
-	Time          int64  // Unix 10位秒时间戳
-	Amount        int64  // 变化金额
-	BalanceBefore int64  // 变化前余额
-	BalanceAfter  int64  // 变化后余额
-	Type          int8   // 1 下注 2 游戏结算
-	GameCode      string // 游戏代码
-	UUID          string // UUID
+	UID           int64  `json:"uid,omitempty"`            // 用户ID
+	Time          int64  `json:"time,omitempty"`           // Unix 10位秒时间戳
+	Amount        int64  `json:"amount,omitempty"`         // 变化金额
+	BalanceBefore int64  `json:"balance_before,omitempty"` // 变化前余额
+	BalanceAfter  int64  `json:"balance_after,omitempty"`  // 变化后余额
+	Type          int8   `json:"type,omitempty"`           // 1 下注 2 游戏结算
+	GameCode      string `json:"game_code,omitempty"`      // 游戏代码
+	UUID          string `json:"uuid,omitempty"`           // UUID
 }
 
 // BalanceChanges 发送单条消息到 Redis,
